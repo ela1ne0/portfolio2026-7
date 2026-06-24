3f3745a21da0aff7c1ad89cd6d12d0d48dc720d8
elaine <elaineyustudio@gmail.com>
2026-06-24 16:27:56 -0400

    modified vite

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,15 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 
 export default defineConfig({
-  base: '/portfolio2026-7',
+  base: '/portfolio2026-7-2/',
+  build: {
+    rollupOptions: {
+      input: {
+        main:  resolve(__dirname, 'index.html'),
+        about: resolve(__dirname, 'about.html'),
+        play:  resolve(__dirname, 'play.html'),
+      }
+    }
+  }
 })
