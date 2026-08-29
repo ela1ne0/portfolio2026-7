cdca3d73ba796730125944390006233a5e78abb5
Elaine Yu <elaineyustudio@gmail.com>
2026-08-29 15:26:35 -0400

    dialogue chnages to play page, added IBM case study + description on thumbnails, fixed text wrapping on thumbnails

--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,7 @@ export default defineConfig({
         play:  resolve(__dirname, 'play.html'),
         work:  resolve(__dirname, 'work/coming-soon.html'),
         hackduke: resolve(__dirname, 'hackduke-2026.html'),
+        ibm: resolve(__dirname, 'ibm-z-devops.html'),
         aboutSoon: resolve(__dirname, 'about-coming-soon.html'),
         trains: resolve(__dirname, 'about-the-trains.html'),
       }
